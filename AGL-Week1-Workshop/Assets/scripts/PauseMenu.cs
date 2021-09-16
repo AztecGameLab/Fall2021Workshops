c9e21d0ae7e6ce987624577483b48c2ffda784cc
CarterAndrews <andrewscart@gmail.com>
2021-09-16 14:54:58 -0700

    Pause Menu

---
+++ AGL-Week1-Workshop/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)|| Input.GetKeyDown(KeyCode.P))
+        {
+            if (PausePanel.active)
+            {
+                UnPause();
+            }
+            else
+            {
+                PausePanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+    public void UnPause()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
