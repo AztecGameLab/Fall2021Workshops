b436ba6eff5d82c6be2b5570855630c37da8e8a1
CarterAndrews <andrewscart@gmail.com>
2021-10-05 18:10:24 -0700

    linrenderer on lasso

--- AGL-Week1-Workshop/Assets/scripts/Lasso.cs
+++ AGL-Week1-Workshop/Assets/scripts/Lasso.cs
@@ -7,6 +7,7 @@ public class Lasso : MonoBehaviour
     public Rigidbody2D playerRb;
     public Rigidbody2D lassoRb;
     public float pullForce;
+    public LineRenderer ropeRendeder;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,8 @@ public class Lasso : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ropeRendeder.SetPosition(0, lassoRb.position);
+        ropeRendeder.SetPosition(1, playerRb.position);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
