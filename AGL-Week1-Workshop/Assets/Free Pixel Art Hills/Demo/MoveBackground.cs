2bdedebd0b069c64b471635d1fa4c8341b63d9a5
CarterAndrews <andrewscart@gmail.com>
2021-10-15 10:38:11 -0700

    parallax in normal scene

--- AGL-Week1-Workshop/Assets/Free Pixel Art Hills/Demo/MoveBackground.cs
+++ AGL-Week1-Workshop/Assets/Free Pixel Art Hills/Demo/MoveBackground.cs
@@ -13,40 +13,20 @@ public class MoveBackground : MonoBehaviour {
 	public float PontoDeDestinoLeft;
 	public float PontoOriginal;
 
-
-
-
-	// Use this for initialization
-	void Start () {
-		//PontoOriginal = transform.position.x;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-
-		
-
-	}
 	public void tick()
     {
-		x = transform.position.x;
-		x += actualSpeed * Time.deltaTime;
-		transform.position = new Vector3(x, transform.position.y, transform.position.z);
-
-
-
+		x = transform.localPosition.x;
+		x += actualSpeed;//* Time.deltaTime;
+		transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
 		if (x <= PontoDeDestino)
 		{
-
-
 			x = PontoOriginal;
-			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+			transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
 		}
 		else if (x >= PontoOriginal)
 		{
 			x = PontoDeDestino;
-			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+			transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
 		}
 	}
 }
--- AGL-Week1-Workshop/Assets/scripts/BackgrondScroller.cs
+++ AGL-Week1-Workshop/Assets/scripts/BackgrondScroller.cs
@@ -7,21 +7,27 @@ public class BackgrondScroller : MonoBehaviour
     public Transform camera;
     public List<MoveBackground> backgroundPieces;
     public float speed;
+    public Transform trackedObject;
     Vector3 lastpos;
+    public float offset;
     // Start is called before the first frame update
     void Start()
     {
         backgroundPieces = new List<MoveBackground>(GetComponentsInChildren<MoveBackground>());
+        transform.position = new Vector3(trackedObject.position.x + offset,transform.position.y,transform.position.z);
         lastpos = transform.position;
     }
     private void Update()
     {
-        foreach(MoveBackground m in backgroundPieces)
+        transform.position = new Vector3(trackedObject.position.x + offset, transform.position.y, transform.position.z);
+        foreach (MoveBackground m in backgroundPieces)
         {
 
-            m.actualSpeed = m.distanceScaler * speed;
+            m.actualSpeed = m.distanceScaler *(transform.position.x-lastpos.x)*speed;
             m.tick();
+
         }
+        lastpos = transform.position;
     }
     // Update is called once per frame
 }
